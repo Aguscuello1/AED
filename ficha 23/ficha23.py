065a600374d47cbf8bab3c8f5e19862d0541bf37
Aguscuello1 <cuelloagustin001@gmail.com>
2025-10-01 21:09:21 -0300

    haciendo la ficha 23

---
+++ ficha 23/ficha23.py
@@ -0,0 +1,8 @@
+#ARCHIVOS
+
+#ARCHIVOS BINARIOS
+m1 = open("datos.dat","w") #lo busca en la carpeta donde tenemos el archivo, modo de apertura(escritura) (grabamos datos)
+m2 = open("c:\\datos.dat","r") #(modo de lectura) 
+
+m1.close()
+m2.close()
